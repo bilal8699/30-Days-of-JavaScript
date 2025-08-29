cccdb17f37076489cc82ac068396fc9594b9a2b0
Muhammad Bilal <muhammadbilal56311@gmail.com>
2025-08-29 16:25:49 +0500

    Add Day01-Basic-Loop exercises

---
+++ Day1 Basic Loop/script.js
@@ -0,0 +1,37 @@
+// ✅ 1. Print numbers from 1 to 100
+
+// For loop
+
+for (i = 1; i <= 100; i++) {
+  // console.log(i);
+}
+
+// While loop
+
+let j = 1;
+while (j <= 100) {
+  // console.log(j);
+  j++;
+}
+
+
+// ✅ 2. Print numbers in reverse (100–1)
+
+for (let i = 100; i >= 1; i--) {
+  // console.log(i);
+}
+
+
+// ✅ 3. Print numbers with a step of 2 (1, 3, 5… up to 100)
+
+//Odd numbers
+
+for (let i = 1; i < 100; i+=2) {
+  // console.log(i);
+}
+
+//Even numbers
+
+for (let i = 2; i <= 100; i+=2) {
+  // console.log(i);
+}
