df955cc30be2f39bca20b5cb82435f36b20c4011
Muhammad Bilal <muhammadbilal56311@gmail.com>
2025-08-29 16:51:50 +0500

    Add Day 2 files

---
+++ Day2 Even Numbers/script.js
@@ -0,0 +1,25 @@
+// 1️⃣ Question: Print even numbers from 1–50
+
+for (let i = 0; i <= 50; i++) {
+  if (i % 2 === 0) {
+    // console.log(i);
+  }
+}
+
+
+// 2️⃣ Question (Extra Practice): Print odd numbers from 1–50
+
+for (let i = 0; i <= 50; i++) {
+  if (i % 2 !== 0) {
+    // console.log(i);
+  }
+}
+
+
+// 3️⃣ Question (Extra Practice): Print multiples of 5 from 1–100
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 5 === 0) {
+    // console.log(i);
+  }
+}
